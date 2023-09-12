29016d57e8eda5eddf34cf480016dcf2ee048b78
fapinter <127316264+fapinter@users.noreply.github.com>
2023-09-12 09:51:56 -0300

    Add files via upload

---
+++ cabral2.py
@@ -0,0 +1,18 @@
+
+x = int(input("Digite um valor para X:"))
+
+funcaoF = 2*x - 4
+
+funcaoG = x**2 - 49
+
+FoG = 2*(funcaoG) - 4
+GoF = (funcaoF)**2 - 49
+FoF = 2*(funcaoF) - 4
+GoG = (funcaoG)**2 - 49
+
+print(f"Funcao FoG {FoG}")
+print(f"funcao GoF {GoF}")
+print(f"funcao FoF {FoF}")
+print(f"funcao GoG {GoG}")
+
+
